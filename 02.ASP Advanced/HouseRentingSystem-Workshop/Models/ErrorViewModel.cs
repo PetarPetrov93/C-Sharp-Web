17495a66f847d1712b530937e645cde55c61e744
PetarPetrov93 <petarpetrov_93@abv.bg>
2024-05-09 14:01:02 +0300

    ASP Advanced workshop
    this is a workshop that will be developed throughout the whole duration of the course.

---
+++ 02.ASP Advanced/HouseRentingSystem-Workshop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HouseRentingSystem_Workshop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
